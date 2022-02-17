34c33ee3a24fe78d353a2cede5322e2197663901
alexandresantosm <amenezes031@gmail.com>
2022-02-17 12:30:20 -0300

    rendering repositories from GitHub API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+
+interface RepositoryDataResponse {
+  name: string;
+  description: string;
+}
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [repositories, setRepositories] = useState<
+    Array<RepositoryDataResponse>
+  >([]);
+
+  useEffect(() => {
+    fetch("http://api.github.com/users/alexandresantosm/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        setRepositories(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Lista de repositórios no GitHub</h1>
+
+      <ul>
+        {repositories.map((repository, index) => (
+          <li key={`${repository.name}_${index}`}>
+            <strong>{repository.name}</strong>
+            <p>{repository.description}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;

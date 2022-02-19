91b590a32f69008a16f5ab81c6077897e4b46fda
alexandresantosm <amenezes031@gmail.com>
2022-02-19 12:29:53 -0300

    create Repository page

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -1,3 +1,35 @@
+import { useQueryClient } from "react-query";
+import { useParams } from "react-router-dom";
+import { Repository } from "../entities/Repository";
+
 export function Repo() {
-  return <h1>Repo</h1>;
+  const params = useParams();
+  const currentRepository = params["*"] as string;
+
+  const queryClient = useQueryClient();
+
+  async function handleChangeRepositoryDescription() {
+    const previousRepos = queryClient.getQueryData<Repository[]>("repos");
+
+    if (previousRepos) {
+      const nextRepos = previousRepos.map((repo) => {
+        if (repo.name === currentRepository) {
+          return { ...repo, description: "Testando" };
+        } else {
+          return repo;
+        }
+      });
+
+      queryClient.setQueryData("repos", nextRepos);
+    }
+  }
+
+  return (
+    <div>
+      <h1>{currentRepository}</h1>
+      <button onClick={handleChangeRepositoryDescription}>
+        Alterar Descrição
+      </button>
+    </div>
+  );
 }
